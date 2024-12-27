9d1eab7af161557bacc2342bb90bfc6fe84a9fd5
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-12-27 13:50:11 -0600

    Update key.js

--- key.js
+++ key.js
@@ -3,4 +3,6 @@ const key = "This Is A Test Key"
 if (document.referrer == null){
   navigator.clipboard.writeText(key)
   alert('copied to clipboard!\n' + key);
+}else{
+  alert('Please Go Through The Loot Link');
 }
