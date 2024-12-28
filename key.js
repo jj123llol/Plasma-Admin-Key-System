649203182b21323c179a6e1fd2e9d90eed68008c
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-12-27 19:24:32 -0600

    Update key.js

--- key.js
+++ key.js
@@ -1,14 +1,8 @@
-const prams = new URLSearchParams(window.location.search)
-const hwid = prams.get("HWID")
-const key = hwid + "This Is A Test Key"
-if (hwid == null){
-  alert('No Hwid Found!');
-}
-else{
-  if (document.referrer == ""){
-    navigator.clipboard.writeText(key)
-    alert('copied to clipboard!\n' + key);
-  }else{
-    alert('Please Go Through The Advertise! Came From: ' + document.referrer);
-  }
+const key = "This Is A Test Key"
+
+if (document.referrer == ""){
+   navigator.clipboard.writeText(key)
+   alert('copied to clipboard!\n' + key);
+}else{
+   alert('Please Go Through The Advertise! Came From: ' + document.referrer);
 }
